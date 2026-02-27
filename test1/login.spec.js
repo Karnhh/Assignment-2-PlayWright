9319a40182b07c0f718f8bfbadda7404408efd98
Hattakorn <hattakorn8895@gmail.com>
2026-02-27 15:50:48 +0700

    run basic command

--- test1/login.spec.js
+++ test1/login.spec.js
@@ -9,6 +9,6 @@ test("test", async ({ page }) => {
   await page.getByRole("textbox", { name: "รหัส PIN 4 หลัก" }).click();
   await page.getByRole("textbox", { name: "รหัส PIN 4 หลัก" }).fill("1234");
   await page.getByRole("button", { name: "เข้าสู่ระบบ" }).click();
-  await expect(page.getByText("เข้าสู่ระบบสำเร็จ")).toBeVisible();
-  // await expect( page.getByText("เข้าสู่ระบบสำเร็จ", { exact: true })).toBeVisible();
+  // await expect(page.getByText("เข้าสู่ระบบสำเร็จ")).toBeVisible();
+  await expect( page.getByText("เข้าสู่ระบบสำเร็จ", { exact: true })).toBeVisible();
 });
